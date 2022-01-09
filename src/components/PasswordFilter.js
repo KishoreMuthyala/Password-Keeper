3c9281e6627dac94f46e35f926c53c200c7b5fe9
KishoreMuthyala <67886934+KishoreMuthyala@users.noreply.github.com>
2022-01-09 13:01:47 +0530

    Added Routing

--- src/components/PasswordFilter.js
+++ src/components/PasswordFilter.js
@@ -2,21 +2,21 @@ import React, { useContext } from "react";
 import PasswordContext from "../context/password/passwordContext";
 
 const PasswordFilter = () => {
-    const passwordContext = useContext(PasswordContext);
-    const { filterPassword } = passwordContext;
-    const changeHandler = (e) => {
-        filterPassword(e.target.value);
-    };
-    return (
-        <div>
-            <input
-                type="search"
-                placeholder="Search..."
-                className="form-control"
-                onChange={changeHandler}
-            />
-        </div>
-    );
+  const passwordContext = useContext(PasswordContext);
+  const { filterPassword } = passwordContext;
+  const changeHandler = (e) => {
+    filterPassword(e.target.value);
+  };
+  return (
+    <div className="mt-2">
+      <input
+        type="search"
+        placeholder="Search..."
+        className="form-control"
+        onChange={changeHandler}
+      />
+    </div>
+  );
 };
 
 export default PasswordFilter;
